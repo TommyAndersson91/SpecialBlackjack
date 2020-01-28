cc98855c9504d2747cd1710e807050a6587fb4e4
TommyAndersson1991 <tommy.andersson@iths.se>
2020-01-28 18:03:11 +0700

    Using Stack for avaible cards, trying to fix two player mode

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class GameLogic : MonoBehaviour
+{
+    public Stack AvaibleCards;
+    int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    public bool firstDraw;
+    public bool player1Turn;
+    public GameObject player1Hand;
+    public GameObject player2Hand;
+    public TextMeshProUGUI myText;
+    public int totalValue;
+    public int totalValue2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     ShuffleArray(ints);
+    player1Hand = GameObject.Find("CardsInHandPanel");
+    player2Hand = GameObject.Find("CardsInHandPanel2");
+    firstDraw = true;
+    player1Turn = true;
+     //För att se blandningen
+    //  for (int i = 0; i < ints.Length; i++)
+    //  {
+    //      Debug.Log(ints[i]);
+    //  }
+        AvaibleCards = new Stack(ints);
+    }
+
+    public void Pass()
+    {
+        player1Turn = !player1Turn;
+
+    }
+
+    public void DrawCards()
+    {
+       
+       
+        GameObject nextCard = GameObject.Find("" + AvaibleCards.Peek());
+        if (player1Turn)
+        {
+            nextCard.transform.SetParent(player1Hand.transform);
+            totalValue += int.Parse(AvaibleCards.Peek().ToString());
+            myText.text = totalValue + " / 21";
+        }
+        else
+        {
+            nextCard.transform.SetParent(player2Hand.transform);
+            totalValue2 += int.Parse(AvaibleCards.Peek().ToString());
+            myText.text = totalValue2 + " / 21";
+        }
+        AvaibleCards.Pop();
+        if (firstDraw)
+        {
+            firstDraw = false;
+            DrawCards();
+        }
+        
+    }
+
+    public static void ShuffleArray(int[] a)
+    {
+        System.Random rand = new System.Random();
+        int length = a.Length;
+        for (int i = 0; i < length; i++)
+        {
+            Swap(a, i, i + rand.Next(length - i));
+        }
+    }
+
+    public static void Swap(int[] arr, int a, int b)
+    {
+        
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,28 @@ using TMPro;
 using System.Linq;
 using System;
 
+
 public class Player : MonoBehaviour
 {
     public ArrayList PlayerCards = new ArrayList();
+    public List<Int32> PlayedCards = new List<Int32>();
     public int randomIndex;
     public int randomIndex2;
     public static int[] theCards = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-    public static List<Int32> CardDeck = new List<Int32>();
+    public List<Int32> CardDeck = new List<Int32>();
     public ArrayList drawnCards;
     public TextMeshProUGUI myText;
     public GameObject card1;
     public GameObject card2;
+    public GameObject card3;
+    public GameObject card4;
+    public GameObject card5;
     public GameObject player1Hand;
     public GameObject player2Hand;
     public System.Random random;
+    public bool firstDraw;
+    public int totalValue;
+   
     
 
 
@@ -30,74 +38,82 @@ public class Player : MonoBehaviour
         {
             CardDeck.Add(i);
         }
-  }
-
-  public void Randomize()
-  {
+        firstDraw = true;
+        var numbers = new List<int>(Enumerable.Range(1, 75));
+        Stack myStack = new Stack();
         
-        int[] drawn = Enumerable.Range(1, 11).OrderBy(x => random.Next()).Take(2).ToArray();
-        // randomIndex = random.Next(0, (CardDeck.Count));
-        // Debug.Log(randomIndex);
-         randomIndex = drawn[0];
-         randomIndex2 = drawn[1];
-        // if (randomIndex == randomIndex2)
-        // {
-        //     // randomIndex = Random.Range(0, 10);
-        //     Randomize();
-        // }
+         
+  }
+   
+        public void Randomize()
+  {
+        if (firstDraw)
+        {
+       
         card1 = GameObject.Find(""+randomIndex);
         card2 = GameObject.Find(""+randomIndex2);
-        // for (int i = 0; i < CardDeck.Count; i++)
-        // {
-        //     if (randomIndex == CardDeck.IndexOf(i))
-        //     {
-        //     CardDeck.RemoveAt(i);
-        //     }
-        // }
+        totalValue = randomIndex+randomIndex2;
+        } else
+        {
+            CheckNumber();
+          GameObject nextCard = GameObject.Find(""+randomIndex);
+          nextCard.transform.SetParent(player1Hand.transform);
+
+        }
+  }
+        
+ public void CheckNumber()
+ {
+        randomIndex = random.Next(CardDeck.Count());
+            PlayedCards.Add(CardDeck.IndexOf(randomIndex));
+            CardDeck.Remove(randomIndex);
+                if (CardDeck.Count < 1)
+                {
+                    myText.text = "No more cards";
+                } 
+ }
+
         // foreach (var item in CardDeck)
         // {
-        //     if (int.Parse(item.ToString()) == randomIndex)
+        //     if (item == randomIndex)
         //     {
         //         CardDeck.Remove(item);
         //     }
         // }
-    }
+    
 
   public void DrawCards()
   {
         Randomize();
         
-        if (player1Hand.transform.childCount > 0)
+        //För att rensa spelarens hand.
+        // if (player1Hand.transform.childCount > 0)
+        // {
+        //     player1Hand.transform.DetachChildren();
+        //     PlayerCards.RemoveRange(0, 2);
+        // }
+        if (firstDraw)
         {
-            player1Hand.transform.DetachChildren();
-            PlayerCards.RemoveRange(0, 2);
-        }
         card1.transform.SetParent(player1Hand.transform);
         card2.transform.SetParent(player1Hand.transform);
-
-     
-
         // transform.parent.GetComponent<Cards>().SetCards();
-        
-    
-        for (int i = 0; i < 2; i++)
+
+            PlayedCards.Add(randomIndex);
+            PlayedCards.Add(randomIndex2);
+
+        firstDraw = false;
+        } else
         {
-            // PlayerCards.Add(Cards.CardDeck[randomIndex]);
-            PlayerCards.Add(randomIndex);
-            PlayerCards.Add(randomIndex2);
-            // CardDeck.RemoveAt(randomIndex);
-            // PlayerCards.Add(Cards.CardDeck[randomIndex2]);
-            // CardDeck.RemoveAt(randomIndex2);
+          totalValue += randomIndex; 
         }
-        
-        
-        int valueCard1 = int.Parse(player1Hand.transform.GetChild(0).name);
-        int valueCard2 = int.Parse(player1Hand.transform.GetChild(1).name);
-        
-       // int valueCard1 = int.Parse(PlayerCards[0].ToString());
-       // int valueCard2 = int.Parse(PlayerCards[1].ToString());
-        // int totalValue = valueCard1 + valueCard2;
-        myText.text = valueCard1+valueCard2 + " / 21";
-        
+       
+        myText.text = totalValue + " / 21";
   }
+
+  public void DrawCard()
+  {
+
+  }
+ 
 }
+
